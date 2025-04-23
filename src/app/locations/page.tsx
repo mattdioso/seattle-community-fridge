fc715517cfc1839e4e17d867bbd5b5ba1e301202
edluo1 <6147723+edluo1@users.noreply.github.com>
2025-04-23 02:01:28 -0700

    Map now updates when buttons are clicked

--- src/app/locations/page.tsx
+++ src/app/locations/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { APIProvider, Map, AdvancedMarker } from "@vis.gl/react-google-maps";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { location_data } from '../data/locations';
 
 export default function Locations() {
@@ -10,6 +10,15 @@ export default function Locations() {
     const [dialogLocation, setDialogLocation] = useState("");
 
     const [markerLocation, setMarkerLocation] = useState(location_data[0]['coord']);
+
+    const INITIAL_CAMERA = {
+        center: markerLocation,
+        zoom: 16
+    };
+
+    const [cameraProps, setCameraProps] = useState<MapCameraProps>(INITIAL_CAMERA);
+    const handleCameraChange = useCallback((ev: MapCameraChangedEvent) => setCameraProps(ev.detail));
+
     return (
         <section>
             <h1 className="text-3xl text-center sm:text-6xl mt-14 font-SauceTomato text-blue-500">Locations</h1>
@@ -17,7 +26,7 @@ export default function Locations() {
                 <div className="w-10/12 mx-auto sm:flex">
                     <div className="h-[250px] sm:h-[500px] w-full sm:w-1/2 border border-black rounded-lg">
                         <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
-                            <Map mapId={"12345"} className="rounded-lg w-full h-full" defaultZoom={16} defaultCenter={markerLocation} gestureHandling={"greedy"} disableDefaultUI>
+                            <Map mapId={"12345"} className="rounded-lg w-full h-full" {...cameraProps} onCameraChanged={handleCameraChange} gestureHandling={"greedy"} disableDefaultUI>
                                 <AdvancedMarker position={markerLocation}>
                                     <img src={logo_img} className="w-9 h-9"/>
                                 </AdvancedMarker>
@@ -34,6 +43,9 @@ export default function Locations() {
                                 </div>
                                 <button className="border-black border bg-blue-500 w-20 h-8 my-auto rounded-lg" onClick={() => {
                                     setMarkerLocation(location.coord)
+                                    const newCameraProps = cameraProps;
+                                    newCameraProps.center = location.coord;
+                                    setCameraProps(newCameraProps);
                                 }}>View</button>
                             </div>
                         ))}
