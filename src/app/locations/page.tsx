eb7f955e07fe38469a36fcc17f3ee83484279ec5
edluo1 <6147723+edluo1@users.noreply.github.com>
2025-06-19 19:05:12 -0700

    Added location description feature to page

--- src/app/locations/page.tsx
+++ src/app/locations/page.tsx
@@ -2,6 +2,7 @@
 import { APIProvider, Map, AdvancedMarker, MapCameraProps, MapCameraChangedEvent } from "@vis.gl/react-google-maps";
 import { useState, useCallback } from "react";
 import { location_data } from '../data/locations';
+import LocationDescription from './LocationDescription';
 
 export default function Locations() {
 
@@ -22,9 +23,9 @@ export default function Locations() {
     return (
         <section>
             <h1 className="text-3xl text-center sm:text-6xl mt-14 font-SauceTomato text-blue-500">Locations</h1>
-            <div className="w-full h-full space-y-16 sm:pt-0 pt-20 mt-0 sm:mt-10">
+            <div className="w-full h-full space-t-16 sm:pt-0 pt-20 mt-0 sm:mt-10">
                 <div className="w-10/12 mx-auto sm:flex">
-                    <div className="h-[250px] sm:h-[500px] w-full sm:w-1/2 border border-black rounded-lg">
+                    <div className="h-[250px] sm:h-[720px] w-full sm:w-1/2 border border-black rounded-lg">
                         <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
                             <Map mapId={"12345"} className="rounded-lg w-full h-full" {...cameraProps} onCameraChanged={handleCameraChange} gestureHandling={"greedy"} disableDefaultUI>
                                 <AdvancedMarker position={markerLocation}>
@@ -33,7 +34,10 @@ export default function Locations() {
                             </Map>
                         </APIProvider>
                     </div>
-                    <div className="w-full sm:w-1/2 bg-white rounded-b-lg sm:rounded-r-lg text-black space-y-6 pt-10">
+                    <div className="w-full sm:w-1/2 bg-white rounded-b-lg sm:rounded-r-lg text-black space-y-6 p-4 pb-10 px-2">
+                        <h2 className="text-2xl font-semibold text-gray-900 sm:text-2xl">
+                            Click "View" next to a fridge location to see details.
+                        </h2>
                         {location_data.map((location, i) => (
                             <div key={`${location}_${i}`} className="flex justify-between px-7">
                                 <div className="block">
@@ -42,7 +46,8 @@ export default function Locations() {
                                     <p>{location.desc ? location.desc : ""}</p>
                                 </div>
                                 <button className="border-black border bg-blue-500 w-20 h-8 my-auto rounded-lg" onClick={() => {
-                                    setMarkerLocation(location.coord)
+                                    setSelectedLocation(location);
+                                    setMarkerLocation(location.coord);
                                     const newCameraProps = cameraProps;
                                     newCameraProps.center = location.coord;
                                     setCameraProps(newCameraProps);
@@ -51,6 +56,9 @@ export default function Locations() {
                         ))}
                     </div>
                 </div>
+                { Object.keys(selectedLocation).length > 0 && 
+                    <LocationDescription name={selectedLocation.name} image={selectedLocation.image} description={selectedLocation.full_description}/>
+                }
             </div>
         </section>
     )
