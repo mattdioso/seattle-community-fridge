c76d12aaf18c61f4e40cce6f4e4a1ee303d8aaff
edluo1 <6147723+edluo1@users.noreply.github.com>
2025-06-19 19:05:12 -0700

    Added location description feature to page

--- src/app/data/locations.js
+++ src/app/data/locations.js
@@ -1,45 +1,76 @@
 export const location_data = [
     {
-        "name": "Woodland Park Presbyterian",
+        "name": "Phinney Ridge",
         "address": "225 N 70th St, Seattle, WA 98103",
+        "desc": "Woodland Park Presbyterian",
+        "full_description": "This fridge is on the corner of Greenwood and 70th in the Phinney Ridge neighborhood. Look for the bus stop on Greenwood on the southwest corner; the fridge is right next to it. This is a full-sized fridge and a medium-sized pantry.",
         "coord": { 
             lat: 47.67947780936359,
             lng: -122.35579540104665
-        }
+        },
+        "image": "/fridge-images/Dunlap-RV 1.jpg"
     },
     {
         "name": "Dunlap/Rainier Beach", 
         "address": "48th Ave S & S Thistle St, Seattle WA 98118",
         "desc": "In alley",
+        "full_description": "This one’s just a little tricky, but well worth it. It’s at 48th Ave S and S Thistle. 48th looks a bit like an alley, but it’s a street. Go just a few yards south on 48th and you will see the full-size fridge on your right with a very large pantry.",
         "coord": {
             lat: 47.52789683813191, 
             lng: -122.27300637540732
-        }
+        },
+        "image": "/fridge-images/Dunlap-RV 1.jpg"
     },
     {
         "name": "North Beacon Hill",
         "address": "1307 13th Ave S, Seattle, WA 98144",
+        "full_description": "Sponsored by our good friends at Queer the Land. This is a mini-fridge and very small pantry.",
         "coord": {
             lat: 47.59123936774281,
             lng: -122.3166102713094
-        }
+        },
+        "image": "/fridge-images/Dunlap-RV 1.jpg"
     },
     {
         "name": "Capitol Hill",
         "address": "745 19th Ave E, Seattle, WA 98112",
         "desc": "St Joseph Parish",
+        "full_description": "The Capitol hill fridge is at 19th and Aloha, on the southwest corner, right against the wall of the church and tucked behind the bus stop. If you stand in the doorway of Macrina Bakery and turn around, you are looking right at it across the street. This is a full-size fridge and small pantry (but we have plans for pantry expansion).",
         "coord": {
             lat: 47.62650480775329,
             lng: -122.30745876505564
-        }
+        },
+        "image": "/fridge-images/Dunlap-RV 1.jpg"
     },
     {
         "name": "LGBTQ+ Center",
-        "address": "400 E Pine, Seattle, WA 98122",
+        "address": "400 E Pine St, Seattle, WA 98122",
         "desc": "Located inside the center",
+        "full_description": "This is our only fridge that’s indoors, and is only available when the center is open. (https://www.gaycity.org/) This is a full-size fridge. The pantry is not stocked by us, but you should be welcome to take food from there.",
         "coord": {
             lat: 47.61554205613653,
             lng: -122.32654650438295
-        }
+        },
+        "image": "https://storage.googleapis.com/seattle-community-fridge/home/e7eb6214-dae2-48e9-9cf3-b3bdee93635d.jpg"
+    },
+    {
+        "name": "Burien",
+        "address": "12027 10th Ave S, Seattle, WA 98168",
+        "full_description": "Managed by a separate group within SCF. Usually lots of food there.",
+        "coord": {
+            lat: 47.495071426540626,
+            lng: -122.32111328435012
+        },
+        "image": "/fridge-images/Dunlap-RV 1.jpg"
+    },
+    {
+        "name": "Estelita’s Library (coming soon)",
+        "address": "241 Martin Luther King Jr Way S, Seattle, WA 98144",
+        "full_description": "We hope to build this sometime in July 2025. Stay tuned! Will include a full-sized fridge, large pantry, street sink, and mutual aid station.",
+        "coord": {
+            lat: 47.600396775707445,
+            lng: -122.2972817120421
+        },
+        "image": "/fridge-images/Dunlap-RV 1.jpg"
     }
 ]
