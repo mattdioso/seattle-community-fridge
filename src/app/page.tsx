526b0952ecc8c052fbfe267af6477196e02aa120
edluo1 <6147723+edluo1@users.noreply.github.com>
2025-08-19 01:10:10 -0700

    Added "upcoming events" to calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         <h1 className="text-3xl sm:text-6xl mt-14 font-SauceTomato text-blue-500">SEATTLE COMMUNITY FRIDGE</h1>
 
         <div className="sm:flex sm:space-x-4 mt-4 px-6 justify-center">
-          <img src={pic} alt="scf_pic"></img>
+          <img src={pic} alt="A refrigerator being set up by two volunteers from Seattle Community Fridge"></img>
             {/* {
               pics.map((pic: string, i: number) => (
                 <div key={i.toString()} className="w-full sm:w-1/4 overflow-x-hidden">
@@ -37,6 +37,17 @@ export default function Home() {
 
               )
             } */}
+        </div>
+        <h2 className="text-2xl sm:text-5xl mt-14 font-SauceTomato text-blue-500">UPCOMING EVENTS</h2>
+        <div className="w-full flex justify-center my-8">
+          <iframe 
+            src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&mode=AGENDA&showTabs=0&showTitle=0&showNav=0&showTz=0&showCalendars=0&showDate=0&showPrint=0&src=NDhiY2FlMDM0ZTc2MzRlNjM2YmYyZmQwMTJhNmY2MTZlMjQ5Y2YxMTlkYzg5Y2QzY2NiNzNkNDgwMzZmMTc4NUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23ef6c00" 
+            style={{ border: 0 }} 
+            width="600" 
+            height="400" 
+            frameBorder="0" 
+            scrolling="no"
+          />
         </div>
       </main>
     </div>
