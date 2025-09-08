cfd9a2bff589042943a7013bc26fab8bd4221e9e
edluo1 <6147723+edluo1@users.noreply.github.com>
2025-09-08 15:32:56 -0700

    Added some things

--- src/app/components/navigation/navbar_v2/index.tsx
+++ src/app/components/navigation/navbar_v2/index.tsx
@@ -78,6 +78,24 @@ export default function Navbar() {
           </div>
         </div>
       </div>
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pt-2 pb-3">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? 'page' : undefined}
+              className={classNames(
+                item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                'block rounded-md px-3 py-2 text-base font-medium',
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
     </Disclosure>
   )
 }
