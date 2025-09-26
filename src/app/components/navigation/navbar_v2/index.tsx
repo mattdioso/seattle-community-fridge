cd0be18b1408fcc0c83abb79d1767b9633662505
edluo1 <6147723+edluo1@users.noreply.github.com>
2025-09-25 20:25:41 -0700

    Added SCF logo, removed components from navbar template

--- src/app/components/navigation/navbar_v2/index.tsx
+++ src/app/components/navigation/navbar_v2/index.tsx
@@ -1,8 +1,10 @@
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { useState, useEffect } from 'react';
 import { SocialIcon } from 'react-social-icons';
 import { hands } from '../../../data/icons';
+import Image from 'next/image'
+import Link from 'next/link'
 
 const navigation = [
   { name: 'SCF', href: '/', current: false },
@@ -57,11 +59,14 @@ export default function Navbar() {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex shrink-0 items-center">
-              <img
-                alt="Your Company"
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-                className="h-8 w-auto"
-              />
+              <Link href={"/"}>
+                <Image 
+                  className="h-10 w-auto"
+                  width={400} height={400}
+                  src="/SCF_logo.png" alt="Seattle Community Fridge"
+                  href="/"
+                />
+              </Link>
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
